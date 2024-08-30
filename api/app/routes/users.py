82564d5b8d901c8c40c6269dbb86d1e616cdced5
Amanda Tinoco <amanda_tinoco00@hotmail.com>
2024-08-30 12:23:20 -0300

    feat: add endpoint questao 2

--- api/app/routes/users.py
+++ api/app/routes/users.py
@@ -11,6 +11,23 @@ router = APIRouter(tags=["Users"])
 def get_all_users(db: Session = Depends(get_db)):
     return db.query(models.User).all()
 
+
+@router.get("/")
+def get_roles(db: Session = Depends(get_db)):
+    return (
+        db.query(
+            models.User.name,
+            models.User.email,
+            models.Roles.description.label("role_description"),
+            models.Claims.description.label("claim_description"),
+        )
+        .join(models.Roles, models.User.role_id == models.Roles.id)
+        .join(models.UserClaims, models.User.id == models.UserClaims.user_id)
+        .join(models.Claims, models.Claims.id == models.UserClaims.claim_id)
+        .all()
+    )
+
+
 @router.get("/{user_id}")
 def get_user_role_by_id(user_id: str, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == user_id).first()
--- questao_2.py
+++
@@ -1,9 +0,0 @@
-results = session.query(
-    user.name,
-    user.email,
-    claims.description.label('claims'),
-    roles.description.label('role')
-).join(user_claims, user.id == user_claims.user_id
-).join(claims, claims.id == user_claims.claim_id
-).join(roles, roles.id == user.role_id
-).all()
